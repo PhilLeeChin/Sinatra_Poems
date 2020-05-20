e1b30f1d7bf4f9e42f58668f1cdfff746f704442
PhilLeeChin <phil.leechin@outlook.com>
2020-05-19 21:29:06 -0400

    users controller to creation and saving of user credentials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
 
   get '/users/new' do #display form to add new users
-    erb ':users/new'
+    erb :'users/new_user'
   end
 
-  post '/' do #add new users to app
+  post '/users' do #add new users to app
     binding.pry
+    new_user = User.new(params)
+
+    if new_user.save
+      session[:user_id] = new_user.id
+      redirect '/poems'
+    else
+      erb :'users/new'
+    end
   end
 end
