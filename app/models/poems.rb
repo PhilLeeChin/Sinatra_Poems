89f7c1e83781f319bfe44bf2573c9c5cc4b38f77
PhilLeeChin <phil.leechin@outlook.com>
2020-05-27 17:24:09 -0400

    validates users

--- app/models/poems.rb
+++ app/models/poems.rb
@@ -1,3 +1,4 @@
 class Poem < ActiveRecord::Base
-
+  belongs_to :user
+  validates :username, :date_created, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
-  validates :email, :password, presence: true
   has_secure_password
+  has_many :poems
+  validates :password, :presence => true,
+                        :confirmation => true,
+                        :length => {:within => 6..12},
+                        :on => :create
+
+  validates :email, uniqueness: true
+
 end
