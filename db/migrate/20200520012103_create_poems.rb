58819f9378fff186140022d7103182c0bbed3211
PhilLeeChin <phil.leechin@outlook.com>
2020-05-27 17:16:57 -0400

    add poem schema, migration

--- db/migrate/20200520012103_create_poems.rb
+++ db/migrate/20200520012103_create_poems.rb
@@ -1,8 +1,12 @@
 class CreatePoems < ActiveRecord::Migration
   def change
     create_table :poems do |p|
-      t.string :title
-      t.string :poem
-      t.string :author
+      p.string :title
+      p.string :poem
+      p.string :author
+      p.string :date_created
+      p.integer :user_id
+      p.timestamps null: false
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200519211056) do
+ActiveRecord::Schema.define(version: 20200520012103) do
+
+  create_table "poems", force: :cascade do |t|
+    t.string   "title"
+    t.string   "poem"
+    t.string   "author"
+    t.string   "date_created"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
