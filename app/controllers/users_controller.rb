dc5177d122900165123d8a02abaaec2033074c77
PhilLeeChin <phil.leechin@outlook.com>
2020-05-27 17:11:37 -0400

    edit routes with if/else condition

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,37 @@
 class UsersController < ApplicationController
-
-  get '/users/new' do #display form to add new users
-    erb :'users/new_user'
+  get '/login' do #display form to add new users
+    erb :'/users/new_user'
   end
 
-  post '/users' do #add new users to app
-    binding.pry
-    new_user = User.new(params)
+  post '/login' do #add new users to app
+    #binding.pry
+    @new_user = User.find_by(username: params[:username])
+    if @new_user && @new_user.authenticate(params[:password])
+      session[:user_id] = @new_user.id
+      redirect to '/poems'
+    else
+      erb :'/errors/login_error'
+    end
+  end
 
-    if new_user.save
-      session[:user_id] = new_user.id
+  get '/register' do
+    erb :'/users/register'
+  end
+
+  post '/register' do
+    @new_user = User.new(username: params[:username],
+    email: params[:email],
+    password: params[:password])
+    if @new_user.save
+      session[:user_id] = @new_user.id
       redirect '/poems'
     else
-      erb :'users/new'
+      erb :'/users/register_error'
     end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
   end
 end
